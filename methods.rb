1f758210a1c1188cdde2b40741b27cf9daba8cf9
sheridan <sheridanmaustad@gmail.com>
2022-02-06 19:45:34 -0700

    commit tree 00385ced3d536921b2deb865d59aacf7dd372ef0

--- methods.rb
+++ methods.rb
@@ -1,1 +1,42 @@
 # Your code here!
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+# const myFunctionReturnValue = myFunction(1);
+# console.log(myFunctionReturnValue);
+
+# def identifies this code as a method
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+# end
+
+# def say_hello()
+#     "hello!"
+#   end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+   num1 + num2
+end
+
+def halve(number)
+    if number.is_a? Integer
+        number / 2
+    else
+        nil
+    end
+    # number / 2;
+end
